da713ed1c4a1e73684a94c45aac228fa78cb31c2
Kushcode3 <kush.bhardwajjj@gmail.com>
2024-10-21 20:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pickle
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
 
-text_transformer = pickle.load(open('text_transformer.pkl','rb'))
+def transform_text(text):
+    text = re.sub('[^a-zA-Z]', ' ', text) # replacing all non-alphabetic characters with a space
+    text = text.lower() # converting to lowecase
+    text = nltk.word_tokenize(text) # to split a string into individual words (tokens)
+
+    filtered_text = []
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation: # removal of stop words and punctuation
+           filtered_text.append(ps.stem(i)) # Stemming
+
+    return " ".join(filtered_text)
+    
 model = pickle.load(open('pipeline_rf_etc_bnb.pkl','rb'))
 
 
@@ -12,7 +29,7 @@ input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
 
     # 1. preprocess
-    transformed_sms = text_transformer([input_sms])
+    transformed_sms = transform_text(input_sms)
     # 2. predict
     result = model.predict(transformed_sms)[0]
     # 3. Display
