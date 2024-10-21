611f10f78f2be162fe1ac8fbbb0cd4b91a879ffb
Kushcode3 <kush.bhardwajjj@gmail.com>
2024-10-21 20:05:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
 
     # 1. preprocess
-    transformed_sms = text_transformer(input_sms)
+    transformed_sms = text_transformer([input_sms])
     # 2. predict
-    result = model.predict(vector_input)[0]
+    result = model.predict(transformed_sms)[0]
     # 3. Display
     if result == 1:
         st.header("Spam")
